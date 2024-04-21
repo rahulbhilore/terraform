96d17b737742c2254db74e92f2a42fba77eb4e30
root <root@ip-172-31-7-60.ap-south-1.compute.internal>
2024-04-21 12:51:27 +0000

    ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "PublicWebTemplate" {
+  ami           = "ami-005e54dee72cc1d00" 
+  instance_type = "t2.micro"
+  subnet_id	= aws.subnet.public-web-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.webserver-security-group.id]
+  key_name	= "demo"
+  user_data	= file("install-apache.sh")
+
+  tags = {
+	Name = "Web"
+	}
+}
+
+resource "aws_instance" "Private-app-template" {
+  ami           = "ami-005e54dee72cc1d00"
+  instance_type = "t2.micro"
+  subnet_id     = aws.subnet.private-app-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.ssh-security-group.id]
+  key_name      = "demo"
+
+
+  tags = {
+        Name = "app"
+        }
+}
+
